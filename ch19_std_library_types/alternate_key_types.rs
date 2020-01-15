eb26765f9eb13f28649575da5d0af37e6254e03c
pprobst <pprobst@insiberia.net>
2020-01-15 13:20:19 -0300

    done std library types

---
+++ ch19_std_library_types/alternate_key_types.rs
@@ -0,0 +1,64 @@
+/*
+ * Any type that implements the Eq and Hash traits can be a key in HashMap. This includes:
+ * - bool (though not very useful since there is only two possible keys).
+ * - int, uint, and all variations thereof.
+ * - String and &str (protip: you can have a HashMap keyed by String and call .get() with an &str).
+ */
+
+use std::collections::HashMap;
+
+// Eq requires that you derive PartialEq on the type.
+#[derive(PartialEq, Eq, Hash)]
+struct Account<'a>{
+    username: &'a str,
+    password: &'a str,
+}
+
+struct AccountInfo<'a>{
+    name: &'a str,
+    email: &'a str,
+}
+
+type Accounts<'a> = HashMap<Account<'a>, AccountInfo<'a>>;
+
+fn try_logon<'a>(accounts: &Accounts<'a>,
+        username: &'a str, password: &'a str){
+    println!("Username: {}", username);
+    println!("Password: {}", password);
+    println!("Attempting logon...");
+
+    let logon = Account {
+        username,
+        password,
+    };
+
+    match accounts.get(&logon) {
+        Some(account_info) => {
+            println!("Successful logon!");
+            println!("Name: {}", account_info.name);
+            println!("Email: {}", account_info.email);
+        },
+        _ => println!("Login failed!"),
+    }
+}
+
+fn main(){
+    let mut accounts: Accounts = HashMap::new();
+
+    let account = Account {
+        username: "j.everyman",
+        password: "password123",
+    };
+
+    let account_info = AccountInfo {
+        name: "John Everyman",
+        email: "j.everyman@email.com",
+    };
+
+    accounts.insert(account, account_info);
+
+    try_logon(&accounts, "j.everyman", "psasword123");
+
+    try_logon(&accounts, "j.everyman", "password123");
+}
+
