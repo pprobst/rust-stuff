2c0a3c7c4d80dc03131bcff3fb14bd452d5b8dc6
pprobst <pprobst@insiberia.net>
2020-01-16 14:43:46 -0300

    done std misc

---
+++ ch20_std_misc/argument_parsing.rs
@@ -0,0 +1,67 @@
+// Matching can be used to parse simple arguments:
+
+use std::env;
+
+fn increase(number: i32) {
+    println!("{}", number + 1);
+}
+
+fn decrease(number: i32) {
+    println!("{}", number - 1);
+}
+
+fn help() {
+    println!("usage:
+match_args <string>
+    Check whether given string is the answer.
+match_args {{increase|decrease}} <integer>
+    Increase or decrease given integer by one.");
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        // No arguments passed.
+        1 => {
+            println!("My name is 'match_args'. Try passing some arguments!");
+        },
+        // One argument passed.
+        2 => {
+            match args[1].parse() {
+                Ok(42) => println!("This is the answer!"),
+                _ => println!("This is not the answer."),
+            }
+        },
+        // One command and one argument passed.
+        3 => {
+            let cmd = &args[1];
+            let num = &args[2];
+            // Parse the number.
+            let number: i32 = match num.parse() {
+                Ok(n) => {
+                    n
+                },
+                Err(_) => {
+                    eprintln!("error: second argument not an integer");
+                    help();
+                    return;
+                },
+            };
+            // Parse the command.
+            match &cmd[..] {
+                "increase" => increase(number),
+                "decrease" => decrease(number),
+                _ => {
+                    eprintln!("error: invalid command");
+                    help();
+                },
+            }
+        },
+        // All the other cases.
+        _ => {
+            // Show a help message.
+            help();
+        }
+    }
+}
